f79375cb0e6b6b899b1190c3603ed1bc6d41b62f
MrVugar <vuqarazizov7777@gmail.com>
2025-02-05 08:05:33 +0000

    task

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,19 @@
+#!/usr/bin/python3
+# 4-inherits_from.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines an inherited class-checking function."""
+
+
 def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
     """
-    Checks if obj is an instance of a class that inherited (directly or indirectly)
-    from the specified class a_class.
-    """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
