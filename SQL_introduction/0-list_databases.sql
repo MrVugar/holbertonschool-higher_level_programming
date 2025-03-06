0489fc4e5ed24841e129b0bb1c1e4438734a1325
MrVugar <vuqarazizov7777@gmail.com>
2025-03-06 07:36:04 +0000

    task

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,1 +1,10 @@
+-- This script lists MySQL databases
+
+-- Initial databases list
 SHOW DATABASES;
+
+-- After creating multiple databases, list them again
+-- CREATE DATABASE test_db1;
+-- CREATE DATABASE test_db2;
+-- SHOW DATABASES;
+
