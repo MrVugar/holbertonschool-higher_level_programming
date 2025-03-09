56b4cbfc56d9cda71799dbc79d00336b769011f4
MrVugar <vuqarazizov7777@gmail.com>
2025-03-09 05:37:17 +0000

    task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,8 @@
--- 1-create_user.sql
-
--- Check if user exists, and only create if it doesn't exist
+-- Check if user already exists, and create user if not
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to ensure that the changes take effect
+FLUSH PRIVILEGES;
